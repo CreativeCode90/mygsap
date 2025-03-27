0bc603343af2ff80d92cfaea7abb7d53b0b48770
CreativeCode90 <jcoder967@gmail.com>
2025-03-27 09:55:08 -0700

    save

--- script/SelectionBar.js
+++ script/SelectionBar.js
@@ -1,43 +1,71 @@
-import { WHeading } from "./widget/Widget.js";
+import { WButton, WContainer, WHeading, Widget } from "./widget/Widget.js";
 
-const selectionStyleObject = {
-    position: 'fixed',
-    right: '10px',
-    bottom: '10px',
-    width: 'auto',
-    padding : '8px 12px',
-    height: '80px',
-    backgroundColor: 'rgb(14, 13, 13)',
-    color: 'white',
-    display :'flex',
-    alignItems : 'center',
-    justifyContent : 'center',
-    fontSize : '24px',
-    borderRadius : '10px',
-    border : '.2px solid rgba(255, 255, 255, 0.322)',
-    cursor: 'pointer',
-
-};
+const widget = new Widget();
 
 export default class SelectionBar {
-    constructor() {
-        this.selectionBar();
-    }
+  constructor() {
+    this.selectionBar();
+  }
 
-    selectionBar(start) {
-        const selectionContainer = document.createElement("div");
-        WHeading(selectionContainer , "Selector bar");
+  selectionBar(start) {
+    const body = document.body;
+    const _MainselectionContaine = WContainer(
+      {
+        position: "fixed",
+        right: "10px",
+        bottom: "10px",
+        width: "auto",
+        padding: "10px",
+        backgroundColor: "black",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row-reverse",
+        cursor: "pointer",
+      },
+      "selectoranimatecontainer"
+    );
+    const selectionContaine = WContainer(
+      {
+        padding: "8px 12px",
+      },
+      "selectionbar"
+    );
+    widget.append(selectionContaine, _MainselectionContaine);
+    const headng = WHeading("Selector bar", {
+      color: "white",
+      fontSize: "2rem",
+    });
+    widget.append(headng, selectionContaine);
+    // add click event
+    selectionContaine.addEventListener("click", () => {
+      document.body.style.cursor = "crosshair";
+      const close = WButton("Stop", {
+        backgroundColor: "transparent",
+        border: "none",
+        color: "white",
+        fontSize: "1.5rem",
+        marginRight: "1rem",
+        backgroundColor: "crimson",
+        padding: "4px 7px 5px",
+      });
+      widget.append(close, _MainselectionContaine);
+      body.addEventListener("click", DetectionAnimateElement, true);
+      // close animation selection
+      close.addEventListener("click", () => {
+        body.removeEventListener("click", DetectionAnimateElement);
+      });
+    });
+    const DetectionAnimateElement = (e) => {
+      if (e.target.id !== "selectoranimatecontainer") {
+        // document.body.removeEventListener("click", DetectionAnimateElement, true);
+        console.log(e.target);
+      }
+    };
 
-        // add click event 
-        selectionContainer.addEventListener("click",()=>{
-            document.body.style.cursor =  'crosshair'
-            selectionContainer.append(closeanimate);
-        })
-        for (const key in selectionStyleObject) {
-            selectionContainer.style[key] = selectionStyleObject[key];
-        }
-        if (start){
-            document.body.appendChild(selectionContainer); // Append to body
-        }
+    if (start) {
+      // document.body.append(_MainselectionContaine); // Append to body
+      widget.append(_MainselectionContaine, body);
     }
+  }
 }
--- script/widget/Widget.js
+++ script/widget/Widget.js
@@ -8,11 +8,53 @@ function ElementCreate(elm ){
     
 }
 
-export function WHeading(nodeElm , title){
+export function WHeading(title , stylesheet){
     let wHeading = ElementCreate("span");
     wHeading.innerText = title ==""? 'Hello': title;
-    wHeading.style.color = 'white';
-    nodeElm.append(wHeading);
+    if (stylesheet== null){
+        wHeading.style.color = 'white'
+    }else{
+        Object.keys(stylesheet).forEach((styleKey) => {
+            wHeading.style[styleKey] = stylesheet[styleKey];
+          });
+        
+    }
     return wHeading;
 }
 
+
+export function WButton(title , stylesheet){
+    let wButton = ElementCreate("button");
+    wButton.innerText = title ==""? 'Hello': title;
+    if (stylesheet== null){
+        wButton.style.color = 'white'
+    }else{
+        Object.keys(stylesheet).forEach((styleKey) => {
+            wButton.style[styleKey] = stylesheet[styleKey];
+          });
+        
+    }
+    return wButton;
+}
+
+
+export function WContainer(stylesheet , id){
+    let wContainer = ElementCreate("div");
+    wContainer.id = id == null ? 'wContainer':id;
+    if (stylesheet== null){
+        wContainer.style.border  = '1px solid red'
+    }else{
+        Object.keys(stylesheet).forEach((styleKey) => {
+            wContainer.style[styleKey] = stylesheet[styleKey];
+          });
+        
+    }
+    return wContainer;
+}
+
+
+export class Widget{
+     append = (elm , nodeElem)=>{
+        return nodeElem.append(elm);
+    }
+}
